85e6ee15ad73af2f64f4cd22e541eb9dde05e0c9
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-01 15:41:05 +0100

    Wire up react router to home, products and contact pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
