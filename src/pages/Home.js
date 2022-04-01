0179b7a4de6e94c782a67e92cc77a79978e9a346
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-01 20:52:25 +0100

    Add background image to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
+import { Box, Grid } from "@mui/material";
+import Background from "../assets/background.jpg";
+
 const Home = () => {
-    return <h1>This is the home page</h1>;
-  };
-  
-  export default Home;
-  
+  return (
+    <Grid
+      container
+      sx={{
+        background: `url(${Background}) no-repeat fixed center`,
+        backgroundPosition: "58% 0",
+        height: "100vh",
+      }}
+    ></Grid>
+  );
+};
+
+export default Home;
