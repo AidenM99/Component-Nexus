21ade48e4e798098a8205f9ff35adffbf31491c9
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-01 20:51:07 +0100

    Add ThemeProvider and customise some styles. Add Header import

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#76B900",
+    },
+    secondary: {
+      main: "#212121",
+    },
+    light: {
+      main: "#f5f5f5",
+    },
+  },
+  mixins: {
+    toolbar: {
+      "@media (min-width: 0px)": {
+        minHeight: "64px",
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
