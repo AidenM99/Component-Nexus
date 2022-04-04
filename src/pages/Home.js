c42d7d315ed9d3ed45479b454edf8b306b29ac5a
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-04 13:58:26 +0100

    Finish home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,50 @@
-import { Box } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import Background from "../assets/images/background.jpg";
 
-const Home = () => {
+const Home = ({ linkStyle }) => {
   return (
-    <Box
+    <Grid
+      container
+      alignItems="center"
       sx={{
         background: `url(${Background}) no-repeat fixed center`,
         backgroundPosition: "58% 0",
         height: "calc(100vh - 4rem)",
         backgroundSize: "cover",
       }}
-    ></Box>
+    >
+      <Grid item xs={12} md={6} px="7.5%" sx={{ color: "#fff", pb: "4rem" }}>
+        <Typography
+          color="primary"
+          variant="h3"
+          textTransform="uppercase"
+          sx={{ fontFamily: "Geforce" }}
+        >
+          GeForce GTX 30 Series
+        </Typography>
+        <Typography
+          color="light"
+          variant="h5"
+          textTransform="uppercase"
+          sx={{ mb: "0.3rem" }}
+        >
+          The Ultimate Play
+        </Typography>
+        <Typography>
+          GeForce RTX™ 30 Series GPUs deliver the ultimate performance for
+          gamers and creators. They're powered by Ampere—NVIDIA's 2nd gen RTX
+          architecture—with new RT Cores, Tensor Cores, and streaming
+          multiprocessors for the most realistic ray-traced graphics and
+          cutting-edge AI features.
+        </Typography>
+        <Link to="/products" style={linkStyle}>
+          <Button variant="contained" sx={{ mt: "0.75rem" }}>
+            Shop Now
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
   );
 };
 
