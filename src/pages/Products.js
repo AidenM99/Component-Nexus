303277f22b7edba3a34f53b933f55657482f4774
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-06 14:53:30 +0100

    Make products page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,31 @@
-const Products = () => {
-  return <h1>This is the products page</h1>;
+import { Box, Grid, Typography } from "@mui/material";
+import ProductCard from "../components/ProductCard";
+import Sidebar from "../components/Sidebar";
+
+const Products = ({ products }) => {
+  return (
+    <Box sx={{ display: "flex", py: "6rem" }}>
+      <Sidebar />
+      <Box sx={{ flexGrow: "1", px: "5%" }}>
+        <Typography
+          color="secondary"
+          variant="h3"
+          textTransform="uppercase"
+          sx={{
+            fontFamily: "Geforce",
+            textDecoration: "underline",
+            textDecorationColor: "#76b900",
+            mb: "1rem",
+          }}
+        >
+          All Items
+        </Typography>
+        <Grid container spacing={3}>
+          <ProductCard products={products} />
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default Products;
