e73ce840204e0f9dc3c1250b7cef7c066d1862e8
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-07 15:55:04 +0100

    Add full filter functionality and partial drawer functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "./components/Header";
+import DrawerComponent from "./components/Drawer";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 import "./assets/fonts/GeForce/GeForce-Bold.ttf";
 import CssBaseline from "@mui/material/CssBaseline";
 import products from "./utils/products/products";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const theme = createTheme({
   palette: {
@@ -35,22 +36,59 @@ const theme = createTheme({
 
 function App() {
   const [productDisplay, setProductDisplay] = useState(products);
+  const [activeFilters, setActiveFilters] = useState([]);
+  const [drawerState, setDrawerState] = useState(false);
+  const notInitialRender = useRef(false);
 
   const linkStyle = {
     textDecoration: "none",
     color: "#f5f5f5",
   };
 
+  useEffect(() => {
+    if (notInitialRender.current) {
+      const newProductDisplay = products.filter((product) =>
+        activeFilters.includes(product.category)
+      );
+
+      activeFilters.length === 0
+        ? setProductDisplay(products)
+        : setProductDisplay(newProductDisplay);
+    } else {
+      notInitialRender.current = true;
+    }
+  }, [activeFilters]);
+
+  const filterProducts = (category, e) => {
+    e.target.checked
+      ? setActiveFilters([...activeFilters, category])
+      : setActiveFilters(activeFilters.filter((filter) => filter !== category));
+  };
+
+  const toggleDrawer = (open) => {
+    console.log("hi");
+    setDrawerState(open);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <Header linkStyle={linkStyle} />
+        <Header linkStyle={linkStyle} toggleDrawer={toggleDrawer} />
+        <DrawerComponent
+          drawerState={drawerState}
+          toggleDrawer={toggleDrawer}
+        />
         <Routes>
           <Route path="/" element={<Home linkStyle={linkStyle} />} />
           <Route
             path="/products"
-            element={<Products products={productDisplay} />}
+            element={
+              <Products
+                products={productDisplay}
+                filterProducts={filterProducts}
+              />
+            }
           />
           <Route path="/contact" element={<Contact />} />
         </Routes>
