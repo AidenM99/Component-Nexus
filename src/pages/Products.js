1d97f7e8ff63817fb3ad0a4b0869f0191cd5e7d8
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-07 15:53:16 +0100

    Styling changes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,28 +2,41 @@ import { Box, Grid, Typography } from "@mui/material";
 import ProductCard from "../components/ProductCard";
 import Sidebar from "../components/Sidebar";
 
-const Products = ({ products }) => {
+const Products = ({ products, filterProducts }) => {
   return (
-    <Box sx={{ display: "flex", py: "6rem" }}>
-      <Sidebar />
-      <Box sx={{ flexGrow: "1", px: "5%" }}>
-        <Typography
-          color="secondary"
-          variant="h3"
-          textTransform="uppercase"
-          sx={{
-            fontFamily: "Geforce",
-            textDecoration: "underline",
-            textDecorationColor: "#76b900",
-            mb: "1rem",
-          }}
-        >
-          All Items
-        </Typography>
-        <Grid container spacing={3}>
-          <ProductCard products={products} />
+    <Box sx={{ background: "#fefefe" }}>
+      <Typography
+        color="secondary"
+        variant="h3"
+        textTransform="uppercase"
+        textAlign="center"
+        sx={{
+          fontFamily: "Geforce",
+          textDecoration: "underline",
+          textDecorationColor: "#76b900",
+          pt: "1.25rem",
+        }}
+      >
+        Our Products
+      </Typography>
+      <Grid
+        container
+        sx={{
+          py: "6rem",
+          px: "2rem",
+          maxWidth: "1420px",
+          margin: "auto",
+        }}
+      >
+        <Grid item xs={2}>
+          <Sidebar filterProducts={filterProducts} />
         </Grid>
-      </Box>
+        <Grid item xs={10} sx={{ flexGrow: "1" }}>
+          <Grid container spacing={3}>
+            <ProductCard products={products} />
+          </Grid>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
