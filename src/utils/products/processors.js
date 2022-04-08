2eaf5e58ce814f02c2a94cfc89438789503a9240
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-08 17:00:41 +0100

    Replace some existing products with new ones

--- src/utils/products/processors.js
+++ src/utils/products/processors.js
@@ -1,41 +1,41 @@
 import uniqid from "uniqid";
-import Ryzen3600 from "../../assets/images/ryzen-3600.jpg";
+import Intel12600K from "../../assets/images/intel-i5-12600k.jpg";
+import Intel12400F from "../../assets/images/intel-i5-12400f.jpg";
 import Ryzen5600X from "../../assets/images/ryzen-5600X.jpg";
-import Ryzen5900X from "../../assets/images/ryzen-5900X.jpg";
 import Ryzen5950X from "../../assets/images/ryzen-5950X.jpg";
 
 const processors = [
   {
     id: uniqid(),
     category: "Processors",
-    name: "AMD Ryzen 3600X",
-    description: "AMD Ryzen 5 3600",
-    price: "£201.50",
-    image: Ryzen3600,
-  },
-  {
-    id: uniqid(),
-    category: "Processors",
     name: "AMD Ryzen 5600X",
-    description: "AMD Ryzen 5 5600X",
-    price: "£210.90",
+    description: "AMD Ryzen 5 5600X 3.7 GHz 6-Core Processor",
+    price: "£211.55",
     image: Ryzen5600X,
   },
   {
     id: uniqid(),
     category: "Processors",
-    name: "AMD Ryzen 5900X",
-    description: "AMD Ryzen 9 5900X",
-    price: "£420.00",
-    image: Ryzen5900X,
+    name: "AMD Ryzen 5950X",
+    description: "AMD Ryzen 9 5950X 3.4 GHz 16-Core Processor",
+    price: "£504.95",
+    image: Ryzen5950X,
   },
   {
     id: uniqid(),
     category: "Processors",
-    name: "AMD Ryzen 5950X",
-    description: "AMD Ryzen 9 5950X",
-    price: "£565.59",
-    image: Ryzen5950X,
+    name: "Intel Core i5-12400F",
+    description: "Intel Core i5-12400F 2.5 GHz 6-Core Processor",
+    price: "£162.99",
+    image: Intel12400F,
+  },
+  {
+    id: uniqid(),
+    category: "Processors",
+    name: "Intel Core i5-12600K",
+    description: "Intel Core i5-12600K 3.7 GHz 10-Core Processor",
+    price: "£258.59",
+    image: Intel12600K,
   },
 ];
 
