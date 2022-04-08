b9fe87efbcd674e9123f8c86c0105bec2d454fe2
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-08 17:01:06 +0100

    Change product card styling

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,8 +5,8 @@ import {
   CardMedia,
   Typography,
   Grid,
-  Button,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ products }) => {
   return (
@@ -14,40 +14,43 @@ const ProductCard = ({ products }) => {
       {products.map((product) => (
         <Grid key={product.id} item xs={8} md={6} lg={4}>
           <Card
-            raised={false}
             sx={{
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-between",
               height: "100%",
+              boxShadow: 0,
+              borderRadius: 0,
             }}
           >
-            <CardActionArea sx={{ padding: "1rem", height: "200px" }}>
-              <CardMedia
-                component="img"
-                image={product.image}
+            <Link to={`/products/${product.name}`} style={{ color: "inherit" }}>
+              <CardActionArea
                 sx={{
-                  width: "100%",
-                  height: "100%",
-                  margin: "auto",
-                  objectFit: "contain",
-                  mixBlendMode: "multiply",
+                  padding: "1rem",
+                  height: "350px",
+                  background: "rgb(242, 242, 242)",
                 }}
-              />
-            </CardActionArea>
-            <CardContent>
-              <Typography variant="h6" fontSize="1rem">
-                {product.name}
-              </Typography>
-              <Typography fontStyle="italic">{product.price}</Typography>
-              <Button
-                variant="contained"
-                fullWidth={true}
-                disableElevation={true}
-                color="secondary"
               >
-                Buy now
-              </Button>
+                <CardMedia
+                  component="img"
+                  image={product.image}
+                  alt="product-image"
+                  sx={{
+                    width: "60%",
+                    maxWidth: "15rem",
+                    height: "100%",
+                    margin: "auto",
+                    objectFit: "contain",
+                    mixBlendMode: "multiply",
+                    "&:hover": { transform: "scale(1.1)" },
+                    transition: "all 0.5s",
+                  }}
+                />
+              </CardActionArea>
+            </Link>
+            <CardContent sx={{ background: "#fff", px: "0", pt: "0.5rem" }}>
+              <Typography fontWeight="700">{product.name}</Typography>
+              <Typography letterSpacing="2px">{product.price}</Typography>
             </CardContent>
           </Card>
         </Grid>
