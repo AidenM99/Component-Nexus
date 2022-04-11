40609bb453678c07815443bddfd10b38e3f6726b
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-11 16:29:38 +0100

    Move nav links into their own component

---
+++ src/components/NavLinks.js
@@ -0,0 +1,24 @@
+import { Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const NavLinks = ({ linkStyle }) => {
+  const pages = ["Home", "Products", "Contact"];
+
+  return (
+    <Box
+      sx={{
+        display: { xs: "none", sm: "none", md: "flex" },
+        justifyContent: "space-between",
+        width: "18.75rem",
+      }}
+    >
+      {pages.map((page) => (
+        <Link key={page} to={page === "Home" ? "/" : page} style={linkStyle}>
+          <Button color="light">{page}</Button>
+        </Link>
+      ))}
+    </Box>
+  );
+};
+
+export default NavLinks;
