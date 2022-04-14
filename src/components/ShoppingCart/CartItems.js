67856d5dbea59223bb2c65b845a8750f995ac0c3
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-14 16:28:04 +0100

    Add reducer function to calculate subtotal of cart items

--- src/components/ShoppingCart/CartItems.js
+++ src/components/ShoppingCart/CartItems.js
@@ -1,107 +1,53 @@
-import {
-  Box,
-  Typography,
-  Grid,
-  TextField,
-  MenuItem,
-  Button,
-} from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-
-const StyledImageContainer = styled(Box)(() => ({
-  background: "rgb(242, 242, 242)",
-  height: "150px",
-  marginRight: "1rem",
-  width: "150px",
-}));
-
-const StyledImage = styled(Box)(() => ({
-  mixBlendMode: "multiply",
-  padding: "1.5rem",
-}));
-
-const StyledItemDetails = styled(Box)(() => ({
-  display: "flex",
-  flex: "1",
-  paddingTop: "1rem",
-}));
-
-const StyledFlexEnd = styled(Box)(() => ({
-  display: "flex",
-  flexGrow: "1",
-  justifyContent: "flex-end",
-}));
+import { useEffect } from "react";
+import CartImage from "./CartImage";
+import CartItemDetails from "./CartItemDetails";
 
 const StyledFlexContainer = styled(Box)(() => ({
   display: "flex",
 }));
 
 const CartItems = ({ cart, handleQuantityChange, removeCartItem }) => {
-  const quantities = [1, 2, 3, 4, 5];
+  const calculateSubtotal = () => {
+    return cart.reduce(
+      (previousValue, currentValue) =>
+        previousValue +
+        parseFloat(currentValue.originalPrice * currentValue.quantity),
+      0
+    );
+  };
 
   return (
     <>
       {cart.map((product) => (
         <Grid item key={product.id} sx={{ my: "0.5rem" }}>
           <StyledFlexContainer>
-            <StyledImageContainer>
-              <StyledImage
-                component="img"
-                src={product.image}
-                alt="product-image"
-                height="100%"
-                width="100%"
-              ></StyledImage>
-            </StyledImageContainer>
-            <StyledItemDetails sx={{ flexDirection: "column" }}>
-              <StyledFlexContainer>
-                <Typography fontWeight={700}>{product.name}</Typography>
-                <StyledFlexEnd>
-                  <Typography letterSpacing="2px">
-                    £
-                    {product.price.toLocaleString(undefined, {
-                      minimumFractionDigits: 2,
-                    })}
-                  </Typography>
-                </StyledFlexEnd>
-              </StyledFlexContainer>
-              <StyledFlexContainer
-                sx={{
-                  flexGrow: "1",
-                  alignItems: "flex-end",
-                }}
-              >
-                <TextField
-                  select
-                  label="Quantity"
-                  value={product.quantity}
-                  onChange={(e) =>
-                    handleQuantityChange(product, e.target.value)
-                  }
-                  sx={{ flex: "1" }}
-                >
-                  {quantities.map((value) => (
-                    <MenuItem key={value} value={value}>
-                      {value}
-                    </MenuItem>
-                  ))}
-                </TextField>
-                <StyledFlexEnd>
-                  <Button
-                    variant="text"
-                    color="secondary"
-                    onClick={() => removeCartItem(product)}
-                  >
-                    <Typography letterSpacing="2px" fontSize="0.75rem">
-                      Remove
-                    </Typography>
-                  </Button>
-                </StyledFlexEnd>
-              </StyledFlexContainer>
-            </StyledItemDetails>
+            <CartImage product={product} />
+            <CartItemDetails
+              product={product}
+              handleQuantityChange={handleQuantityChange}
+              removeCartItem={removeCartItem}
+            />
           </StyledFlexContainer>
         </Grid>
       ))}
+      <StyledFlexContainer sx={{ justifyContent: "flex-end" }}>
+        <Typography letterSpacing="1px" fontWeight="600">
+          Subtotal: £
+          {calculateSubtotal().toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+          })}
+        </Typography>
+      </StyledFlexContainer>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        sx={{ mt: "1rem" }}
+      >
+        Checkout
+      </Button>
     </>
   );
 };
