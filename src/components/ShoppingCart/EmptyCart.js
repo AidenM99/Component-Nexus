9c88d22d5c56836c06608b66c4bd19c1908944a8
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-14 15:15:29 +0100

    Add empty cart component

---
+++ src/components/ShoppingCart/EmptyCart.js
@@ -0,0 +1,44 @@
+import { Box, Button, Typography } from "@mui/material";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+
+const Container = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  flexGrow: 1,
+}));
+
+const StyledBox = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "center",
+}));
+
+const EmptyCart = ({ toggleDrawer }) => {
+  return (
+    <Container>
+      <Box>
+        <Typography fontWeight={500} fontStyle="italic">
+          Your cart is empty.
+        </Typography>
+      </Box>
+      <StyledBox>
+        <ShoppingBagIcon color="disabled" sx={{ fontSize: 250 }} />
+      </StyledBox>
+      <StyledBox>
+        <Link to={"/products"} style={{ textDecoration: "none" }}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => toggleDrawer(false)}
+          >
+            Browse Products
+          </Button>
+        </Link>
+      </StyledBox>
+    </Container>
+  );
+};
+
+export default EmptyCart;
