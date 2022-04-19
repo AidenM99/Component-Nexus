9a64ed37de874c42e4bf8bc0e8dbf1f11ba6be57
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-19 16:57:49 +0100

    Styling changes

--- src/components/ShoppingCart/CartItemDetails.js
+++ src/components/ShoppingCart/CartItemDetails.js
@@ -1,29 +1,62 @@
 import { styled } from "@mui/system";
 import { Box, Typography, TextField, MenuItem, Button } from "@mui/material";
 
-const StyledFlexContainer = styled(Box)(() => ({
-  display: "flex",
-}));
-
 const StyledItemDetails = styled(Box)(() => ({
   display: "flex",
   flex: "1",
-  paddingTop: "1rem",
 }));
 
-const StyledFlexEnd = styled(Box)(() => ({
+const ProductDetailsOne = styled(Box)(({ theme }) => ({
+  display: "flex",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+}));
+const ProductDetailsTwo = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexGrow: "1",
+  alignItems: "flex-end",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column-reverse",
+  },
+}));
+
+const StyledFlexEnd = styled(Box)(({ theme }) => ({
   display: "flex",
   flexGrow: "1",
   justifyContent: "flex-end",
+  [theme.breakpoints.down("sm")]: {
+    justifyContent: "flex-start",
+  },
 }));
 
-const CartItemDetails = ({ product, removeCartItem, handleQuantityChange }) => {
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  flex: "1",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+  },
+}));
+
+const StyledButton = styled(Button)(() => ({
+  minHeight: 0,
+  minWidth: 0,
+  padding: 0,
+}));
+
+const CartItemDetails = ({
+  product,
+  setItemLimit,
+  removeCartItem,
+  handleQuantityChange,
+}) => {
   const quantities = [1, 2, 3, 4, 5];
 
   return (
     <StyledItemDetails sx={{ flexDirection: "column" }}>
-      <StyledFlexContainer>
-        <Typography fontWeight={700}>{product.name}</Typography>
+      <ProductDetailsOne>
+        <Typography fontWeight={700} sx={{ mr: "0.5rem" }}>
+          {product.name}
+        </Typography>
         <StyledFlexEnd>
           <Typography letterSpacing="2px">
             £
@@ -32,38 +65,39 @@ const CartItemDetails = ({ product, removeCartItem, handleQuantityChange }) => {
             })}
           </Typography>
         </StyledFlexEnd>
-      </StyledFlexContainer>
-      <StyledFlexContainer
-        sx={{
-          flexGrow: "1",
-          alignItems: "flex-end",
-        }}
-      >
-        <TextField
+      </ProductDetailsOne>
+      <ProductDetailsTwo>
+        <StyledTextField
           select
           label="Quantity"
           value={product.quantity}
           onChange={(e) => handleQuantityChange(product, e.target.value)}
-          sx={{ flex: "1" }}
         >
           {quantities.map((value) => (
             <MenuItem key={value} value={value}>
               {value}
             </MenuItem>
           ))}
-        </TextField>
-        <StyledFlexEnd>
-          <Button
+        </StyledTextField>
+        <StyledFlexEnd
+          sx={{
+            width: { xs: "100%", sm: "auto" },
+          }}
+        >
+          <StyledButton
             variant="text"
             color="secondary"
-            onClick={() => removeCartItem(product)}
+            onClick={() => {
+              removeCartItem(product);
+              setItemLimit(false);
+            }}
           >
             <Typography letterSpacing="2px" fontSize="0.75rem">
               Remove
             </Typography>
-          </Button>
+          </StyledButton>
         </StyledFlexEnd>
-      </StyledFlexContainer>
+      </ProductDetailsTwo>
     </StyledItemDetails>
   );
 };
