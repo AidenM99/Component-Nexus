975e6f8522a6a39068eb8f683aeb4d64d52c9537
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-20 17:34:50 +0100

    Make CartButton component

---
+++ src/components/Navbar/CartButton.js
@@ -0,0 +1,20 @@
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Box, Badge, IconButton } from "@mui/material";
+
+const CartMenu = ({ toggleCartDrawer, cart }) => {
+  return (
+    <Box>
+      <IconButton
+        color="light"
+        sx={{ justifySelf: "flex-end" }}
+        onClick={() => toggleCartDrawer(true)}
+      >
+        <Badge badgeContent={cart.length} color="primary">
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+    </Box>
+  );
+};
+
+export default CartMenu;
