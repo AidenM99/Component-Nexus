91cf6b8defa7516cb3faec1b3f493670a0762215
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-04-20 17:38:24 +0100

    Add icon imports

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,3 +1,8 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
 const Contact = () => {
   return <h1>This is the contact page</h1>;
 };
